dcd2557d028074b2a61dbe7f05b445f9b5ec4836
Benaventi Fuentes <Benaventi Fuentes>
2016-12-07 08:21:21 -0800

    First Commit Add Hello World

---
+++ src/main/java/edu/ipc2/usac/ipc2vd2016/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.ipc2.usac.ipc2vd2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean textSet =false;
+    public void onClick1(View v) {
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        if (!textSet){
+            text1.setText("Bonjour tout le monde");
+        }else {text1.setText("");}
+
+
+    }
+}
